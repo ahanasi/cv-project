e466c44a2d6a170f8b69947ff3e13fdaff5c2818
XXX Ahana <ahanasi@seas.upenn.edu>
2021-11-02 19:16:05 -0700

    Create component skeleton

---
+++ src/components/Experience.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { v4 as uuidv4 } from "uuid";
+
+class Experience extends Component {
+  constructor(props) {
+    super(props);
+  }
+}
+
+export default Experience;
